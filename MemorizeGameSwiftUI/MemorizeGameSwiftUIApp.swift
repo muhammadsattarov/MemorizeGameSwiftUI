1b317758fc4a8e6f13f89ff839dd170c85f1cdc0
Muhammad Sattarov <sattarovmuhammad98@gmail.com>
2024-08-10 17:26:37 +0500

    Initial Commit

---
+++ MemorizeGameSwiftUI/MemorizeGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameSwiftUIApp.swift
+//  MemorizeGameSwiftUI
+//
+//  Created by user on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
