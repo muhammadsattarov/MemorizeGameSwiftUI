2494296a414213a116907a3396b4812f5e250668
Muhammad Sattarov <sattarovmuhammad98@gmail.com>
2024-08-10 17:36:22 +0500

    Create CardView

--- MemorizeGameSwiftUI/ContentView.swift
+++ MemorizeGameSwiftUI/ContentView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+  let emojis = ["🐼", "🕸️", "👻", "🎃"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+          ForEach(emojis.indices, id: \.self) { index in
+            CardView(content: emojis[index])
+          }
         }
+        .foregroundStyle(.orange)
         .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CardView: View {
+  let content: String
+  @State var isFaceUp = true
+  var body: some View {
+    ZStack {
+      let base = RoundedRectangle(cornerRadius: 12)
+      if isFaceUp {
+        base.fill(.white)
+        base.strokeBorder(lineWidth: 2)
+        Text(content).font(.largeTitle)
+      } else {
+        base.fill()
+      }
+    }
+    .onTapGesture {
+      isFaceUp.toggle()
+    }
+  }
 }
